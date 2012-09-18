81dd0cc59649ea3af2d866ffd9c9e3ce85525df1
Andy Clarke <andy.uq@gmail.com>
2012-09-18 16:56:35 +1200

    Initial commit

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace BowlingKata
+{
+	public class Game
+	{
+		private int[] _rolls = new int[21];
+		private int _currentRoll = 0;
+
+		public void Roll(int numPins)
+		{
+			if (_currentRoll > 20)
+				throw new InvalidOperationException("This game is already complete");
+
+			_rolls[_currentRoll] = numPins;
+			_currentRoll++;
+		}
+
+		public int Score()
+		{
+			int score = 0;
+			int frameIndex = 0;
+			for (var frame =0; frame < 10; frame++ )
+			{
+				int frameScore;
+				if ( IsStrike(frameIndex) )
+				{
+					frameScore = 10 + StrikeBonus(frameIndex);
+					frameIndex++;
+				}
+				else
+				{
+
+					frameScore = SumOfFrame(frameIndex);
+					if (IsSpare(frame))
+					{
+						frameScore += SpareBonus(frameIndex);
+					}
+
+					frameIndex += 2;
+				}
+
+				score += frameScore;
+			}
+
+			return score;
+		}
+
+		private int SumOfFrame(int frameIndex)
+		{
+			return _rolls[frameIndex] + _rolls[frameIndex + 1];
+		}
+
+		private int SpareBonus(int frameIndex)
+		{
+			int spareBonus = _rolls[frameIndex + 2];
+			return spareBonus;
+		}
+
+		private int StrikeBonus(int frameIndex)
+		{
+			var strikeBonus = _rolls[frameIndex + 1]
+			                  + _rolls[frameIndex + 2];
+			return strikeBonus;
+		}
+
+		private bool IsStrike(int frameIndex)
+		{
+			return _rolls[frameIndex] == 10;
+		}
+
+		private bool IsSpare(int frame)
+		{
+			return _rolls[frame] + _rolls[frame + 1] == 10;
+		}
+	}
+}
---
+++ BowlingKataTests/BowlingGameTest.cs
@@ -0,0 +1,85 @@
+﻿using BowlingKata;
+using NUnit.Framework;
+
+namespace BowlingKataTests
+{
+	[TestFixture]
+	public class BowlingGameTest
+	{
+
+		[SetUp]
+		public void SetUp()
+		{
+			_game = new Game();
+		}		private Game _game;
+
+		[Test]
+		public void TestGutterGame()
+		{
+			RollMany(0, 20);
+
+			int score = _game.Score();
+			Assert.That(score, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void TestAllOnes()
+		{
+			RollMany(1, 20);
+
+			int score = _game.Score();
+			Assert.That(score, Is.EqualTo(20));
+		}
+
+		[Test]
+		public void TestRollOneSpare()
+		{
+			RollSpare();
+			_game.Roll(3);
+			RollMany(0, rollCount: 17);
+
+			Assert.That(_game.Score(), Is.EqualTo(16));
+		}
+
+		[Test]
+		public void TestRollStrike()
+		{
+			RollStrike();
+			_game.Roll(3);
+			_game.Roll(4);
+			RollMany(0, rollCount:16);
+
+			Assert.That(_game.Score(), Is.EqualTo(24));
+		}
+
+		[Test]
+		public void PerfectGame()
+		{
+			RollMany(10, rollCount:12);
+			Assert.That(_game.Score(), Is.EqualTo(300));
+		}
+
+		[Test,ExpectedException(ExpectedException = typeof(System.InvalidOperationException))]
+		public void NoMoreThan21Rolls()
+		{
+			RollMany(0, 22);
+		}
+
+		private void RollStrike()
+		{
+			_game.Roll(10);
+		}
+
+		private void RollSpare()
+		{
+			_game.Roll(3);
+			_game.Roll(7);
+		}
+
+		private void RollMany(int numPins, int rollCount)
+		{
+			for (int i = 0; i < rollCount; i++)
+				_game.Roll(numPins);
+		}
+	}
+}
